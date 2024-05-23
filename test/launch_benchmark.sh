9fb492d2e6bf14853e9920feb9661977e19fef8d
linke <linke@pplabs.org>
2024-05-23 09:54:00 +0800

    Update the vllm image to  "ppinfer_vllm:latest" and add "--log-stats"
    Signed-off-by: linke <linke@pplabs.org>

--- test/launch_benchmark.sh
+++ test/launch_benchmark.sh
@@ -19,6 +19,7 @@ MAX_NUM_SEQS=128
 MAX_SEQ_LEN=4096
 MAX_BATCHED_TOKEN=4096
 PROMPT_POLICY="fixed"
+ENABLE_LOG_STATS=0
 
 #MODEL_LIST=("Mixtral-8x7B-Instruct-v0.1" "Toppy-M-7B mistral_7b" "MythoMax-L2-13b" "lzlv_70b_fp16_hf")
 MODEL_LIST=("Meta-Llama-3-8B-Instruct")
@@ -59,7 +60,8 @@ function run() {
         check_trtllm $model_path $trt_engine_path $trt_ifb_path
         extra_opts="--trt-engine-dir $trt_engine_path --trt-ifb-dir $trt_ifb_path"
     elif [ "$BACKEND" = "vllm" ]; then
-        image_tag="ppinfer/vllm-openai:v0.4.2"
+        image_tag="ppinfer_vllm:latest"
+#        image_tag="ppinfer/vllm-openai:v0.4.2"
 #        image_tag="vllm/vllm-openai:v0.4.0"
     elif [ "$BACKEND" = "mii" ]; then
         image_tag="ppinfer_mii:0.1"
@@ -80,7 +82,7 @@ function run() {
             --model-dir $model_path --data-dir $DEF_DATA_DIR --cuda-devices $CUDA_DEVICES --tp $TP --pp $PP --memory-fraction $MEM_FRACTION \
             --max-num-seq $MAX_NUM_SEQS --max-seq-len $MAX_SEQ_LEN --max-batched-tokens $MAX_BATCHED_TOKEN --prompt-policy $PROMPT_POLICY \
             --warmup-reqs $warmup_reqs --norm-reqs $norm_reqs --concurrent-reqs $concur --prompt-policy fixed \
-            --input-len $input_len --output-len $output_len $extra_opts --fp8 $FP8_SUPPORT
+            --input-len $input_len --output-len $output_len $extra_opts --fp8 $FP8_SUPPORT --log-stats $ENABLE_LOG_STATS
     done
 }
 
@@ -90,6 +92,7 @@ function usage() {
     LOG INFO "    --backend    Specify the backend from: trtllm, vllm, tgi, siliconllm, mii"
     LOG INFO "    --model      Special model to load, if not set, use preset models"
     LOG INFO "    --fp8        How to config fp8 inference, its value should be: weight, kvcache, all"
+    LOG INFO "    --log-stats  Enable vllm to log stats during inference"
     exit
 }
 
@@ -117,6 +120,10 @@ function main() {
         shift
         FP8_SUPPORT="$1"
         shift
+        ;;
+    --log-stats)
+        shift
+        ENABLE_LOG_STATS=1
         ;;
     *)
         usage
