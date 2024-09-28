ba463321f7a42c059f7f7ae719ffeab0be43b1c3
linke <linke@pplabs.org>
2024-09-28 12:47:42 +0800

    Update the llm api demo script
    Signed-off-by: linke <linke@pplabs.org>

--- test/llm_api_demo.py
+++ test/llm_api_demo.py
@@ -5,18 +5,18 @@ import os
 import sys
 import traceback
 import time
-import warnings
 import aiohttp
 import requests
+from urllib.parse import urlparse
 from loguru import logger
 from tqdm.asyncio import tqdm
 from dataclasses import dataclass, field
-from datetime import datetime
 from typing import AsyncGenerator, List, Optional, Tuple
 
 AIOHTTP_TIMEOUT = aiohttp.ClientTimeout(total=6 * 60 * 60)
 
-VLLM_ENDPOINTS = {
+"""
+Example of APIs:
     "version": "/version",
     "health": "/health",
     "models": "/v1/models",
@@ -24,30 +24,28 @@ VLLM_ENDPOINTS = {
     "chat-completions": "/v1/chat/completions",
     "embeddings": "/v1/embeddings",
     "compatible-embeddings": "/compatible-mode/v1/embeddings"
-}
+"""
 
 @dataclass
-class RequestInput:
-    prompt: str
-    api_url: str
-    prompt_len: int = 0
-    output_len: int = 0
-    model: str = ""
-    stream: bool = True
-    best_of: Optional[int] = None
-    use_beam_search: Optional[bool] = None
-    ignore_eos: Optional[bool] = None
-
-@dataclass
-class RequestOutput:
-    generated_text: str = ""
-    success: bool = False
-    latency: float = 0.0
-    ttft: float = 0.0  # Time to first token
-    itl: List[float] = field(
-        default_factory=list)  # List of inter-token latencies
-    prompt_len: int = 0
-    error: str = ""
+class CompletionContext:
+    index: int = field(default=0)
+    # input
+    model: str = field(default="")
+    prompt: str = field(default="")
+    prompt_len: int = field(default=0)
+    max_tokens: int = field(default=10)
+    stream: Optional[bool] = field(default=False)
+    ignore_eos: Optional[bool] = field(default=False)
+    # output
+    error: Optional[str] = field(default=None)
+    generated: str = field(default="")
+    output_len: int = field(default=0)
+    e2e_latency: float = field(default=0)
+    decode_latency: float = field(default=0)
+    ttft: Optional[float] = field(default=None)
+    tpot: Optional[float] = field(default=None)
+    itl: Optional[float] = field(default=None)
+    tps: float = field(default=0)
 
 @dataclass
 class EmbeddingOutput:
@@ -78,60 +76,60 @@ def print_chunk_streamly(count: int, data):
         sys.stdout.write(cur)
         sys.stdout.flush()
 
-async def async_request_openai_completions(
-    action: str,
-    request_input: RequestInput,
-    api_key: Optional[str] = None,
-    pbar: Optional[tqdm] = None,
-) -> RequestOutput:
-    api_url = request_input.api_url
+async def async_request_openai_completions(ctx: CompletionContext, url: str, api_key: Optional[str] = None, pbar: Optional[tqdm] = None):
+    payload = {
+        "model": ctx.model,
+        "max_tokens": ctx.max_tokens,
+    }
+    if ctx.stream is not None:
+        payload["stream"] = ctx.stream
+    if ctx.ignore_eos is not None:
+        payload["ignore_eos"] = ctx.ignore_eos
+    chat = "/chat/completions" in url
+    if os.path.isfile(ctx.prompt):
+        print(f"Load prompt from {ctx.prompt}")
+        with open(ctx.prompt, "r") as f:
+            data = f.read()
+            if ctx.prompt.endswith(".json") or ctx.prompt.endswith(".jsonl"):
+                data = json.loads(data)
+                if "prompt" in data or "messages" in data:
+                    for k in data:
+                        payload[k] = data[k]
+                else:
+                    raise ValueError(f"Invalid content in file {ctx.prompt}")
+            else:
+                if chat:
+                    payload["messages"] = []
+                    payload["messages"].append({"role": "user", "content": data})
+                else:
+                    payload["prompt"] = data
+    else:
+        if chat:
+            payload["messages"] = []
+            payload["messages"].append({"role": "user", "content": args.prompt})
+        else:
+            payload["prompt"] = args.prompt
 
+    ## more parameters
+    #payload["temperature"] = 0.2
+    #payload["top_k"] = 5
+    #payload["frequency_penalty"] = 0.5
+    #payload["presence_penalty"] = 0.1
+    #payload["max_tokens"] = 30
+
+    headers = {"User-Agent": "LLM API Client"}
+    #headers["accept"] = "application/json"
+    #headers["content-type"] = "application/json"
+    if api_key is not None and api_key != "":
+        headers["Authorization"] = "Bearer " + api_key
+
+    print(f"URL: {url}, Payload: {payload}")
+    st_start = time.perf_counter()
+    most_recent_timestamp = st_start
     async with aiohttp.ClientSession(timeout=AIOHTTP_TIMEOUT) as session:
-        assert not request_input.use_beam_search
-        if os.path.isfile(request_input.prompt) and os.path.exists(request_input.prompt):
-            with open(request_input.prompt, "r") as f:
-                request_input.prompt = f.read()
-                logger.info(f"Load prompt from file: {request_input.prompt}")
-        payload = {
-            "model": request_input.model,
-            #"temperature": 0.8,
-            "max_tokens": request_input.output_len,
-            "stream": request_input.stream,
-        }
-        if request_input.best_of is not None:
-            payload["best_of"] = request_input.best_of
-        if request_input.ignore_eos is not None:
-            payload["ignore_eos"] = request_input.ignore_eos
-        if action == "completions":
-            payload["prompt"] = request_input.prompt
-        elif action == "chat-completions":
-            payload["messages"] = [{"role": "user", "content": request_input.prompt}]
-
-        ## more parameters
-        #payload["temperature"] = 0.2
-        #payload["top_k"] = 5
-        #payload["frequency_penalty"] = 0.5
-        #payload["presence_penalty"] = 0.1
-        #payload["max_tokens"] = 30
-
-        headers = {"User-Agent": "LLM API Client"}
-        #headers["accept"] = "application/json"
-        #headers["content-type"] = "application/json"
-        if api_key is not None and api_key != "":
-            headers["Authorization"] = "Bearer " + api_key
-
-        output = RequestOutput()
-        output.prompt_len = request_input.prompt_len
-
-        generated_text = ""
-        ttft = 0.0
-        latency = 0.0
-        st = time.perf_counter()
-        most_recent_timestamp = st
-        print(f"URL:{api_url}, payload: {payload}")
         try:
             iter = 0
-            async with session.post(url=api_url, json=payload, headers=headers) as response:
+            async with session.post(url=url, json=payload, headers=headers) as response:
                 if response.status == 200:
                     async for chunk_bytes in response.content:
                         chunk_bytes = chunk_bytes.strip()
@@ -142,10 +140,10 @@ async def async_request_openai_completions(
                         if chunk == ": OPENROUTER PROCESSING":
                             continue
                         if chunk == "[DONE]":
-                            latency = time.perf_counter() - st
+                            ctx.e2e_latency = time.perf_counter() - st_start
                         else:
                             data = json.loads(chunk)
-                            if request_input.stream:
+                            if ctx.stream:
                                 print_chunk_streamly(iter, data)
                             text = None
                             if "choices" in data:
@@ -161,33 +159,26 @@ async def async_request_openai_completions(
                             if text is not None:
                                 timestamp = time.perf_counter()
                                 # First token
-                                if ttft == 0.0:
-                                    ttft = time.perf_counter() - st
-                                    output.ttft = ttft
+                                if ctx.ttft is None:
+                                    ctx.ttft = timestamp - st_start
                                 # Decoding phase
                                 elif data.get("usage", None) is None:
-                                    output.itl.append(timestamp - most_recent_timestamp)
-
+                                    ctx.itl =  [] if ctx.itl is None else ctx.itl
+                                    ctx.itl.append(timestamp - most_recent_timestamp)
                                 most_recent_timestamp = timestamp
-                                generated_text += text
-
-                    output.generated_text = generated_text
-                    output.success = True
-                    output.latency = latency
-                    if not request_input.stream:
-                        print(output.generated_text)
+                                ctx.generated += text
+                    if not ctx.stream:
+                        print(ctx.generated)
+                    if ctx.e2e_latency < 0.0001:
+                        ctx.e2e_latency = time.perf_counter() - st_start
                 else:
-                    output.success = False
                     details = await response.text()
-                    output.error = f"status code {response.status}({response.reason}).\n\t Details: {details}."
+                    ctx.error = f"status code {response.status}({response.reason}).\n\t Details: {details}."
         except Exception:
-            output.success = False
             exc_info = sys.exc_info()
-            output.error = "".join(traceback.format_exception(*exc_info))
-
+            ctx.error = "".join(traceback.format_exception(*exc_info))
     if pbar:
         pbar.update(1)
-    return output
 
 ## reference: https://help.aliyun.com/zh/model-studio/developer-reference/embedding-interfaces-compatible-with-openai
 async def async_generate_embeddings(url: str, input: str, model_name: str, api_key: str = None) -> EmbeddingOutput:
@@ -220,144 +211,82 @@ async def async_generate_embeddings(url: str, input: str, model_name: str, api_k
         return output
 
 def check_health(url: str, ) -> bool:
-    try:
-        response = requests.get(url, timeout=10)
-        if response.status_code == 200:
-            return True
-    except:
-        pass
-    return False
+    response = requests.get(url, timeout=10)
+    if response.status_code == 200:
+        return True
+    else:
+        return False
 
 def get_version(url: str):
-    try:
-        res = requests.get(url, timeout=10)
-        if res.status_code == 200:
-            return res.text
-        else:
-            print(f"get_version: {res}")
-    except e:
-        print(f"Exception is raised: {e}")
-    return ""
+    res = requests.get(url, timeout=10)
+    if res.status_code == 200:
+        return res.json().get("version")
+    else:
+        raise RuntimeError(f"Failed to get version({res.status_code}): {res.text()}")
 
-def exec_get_method(args):
-    try:
-        headers = {"User-Agent": "LLM API Client"}
-        if args.api_key is not None and args.api_key != "":
-            headers["Authorization"] = "Bearer " + args.api_key
-        res = requests.get(args.url + args.path, headers=headers, timeout=10)
-        if res.status_code == 200:
-            return res.text
-        else:
-            print(f"Response: {res}")
-    except e:
-        print(f"Exception is raised: {e}")
-    return ""
+def get_model(url: str, headers = None)->Optional[str]:
+    res = requests.get(url, headers = headers)
+    model_list = res.json().get("data", [])
+    return model_list[0]["id"] if model_list else None
 
-
-def list_models(url: str):
-    try:
-        res = requests.get(url, timeout=10)
-        if res.status_code == 200:
-            return res.text
-        else:
-            print(f"list_models: {res}")
-    except e:
-        print(f"Exception is raised: {e}")
-    return ""
+def get_model_list(url: str, headers = None)->Optional[List[str]]:
+    res = requests.get(url, headers = headers)
+    model_list = res.json().get("data", [])
+    if model_list and len(model_list) > 0:
+        models = []
+        for m in model_list:
+            models.append(m["id"])
+        return models
+    else:
+        return None
 
 def main(args):
-    if args.backend != "vllm":
-        print("Only support vllm")
-        return
-    action = None
-    for k, v in VLLM_ENDPOINTS.items():
-        if args.path == v:
-            action = k
-            break
-    #if not action:
-    #    print(f"Invalid path: {args.path}")
-    #    return
-    url = args.url + args.path
-    print(f"Request: {url}")
+    print(f"Request: {args.url}")
+    headers = {"User-Agent": "LLM API Client"}
+    if args.api_key is not None and args.api_key != "":
+        headers["Authorization"] = "Bearer " + args.api_key
+    parsed_url = urlparse(args.url)
+    if args.model_name is None:
+        args.model_name = get_model(args.url[0:-len(parsed_url.path)] + "/v1/models")
+    print(f"Model name: {args.model_name}")
 
-    if action == "health":
-        if check_health(url):
-            print("Health check is passed")
-        else:
-            print("Health check is failed")
-    elif action == "version":
-        ver_str = get_version(url)
-        if ver_str:
-            ver = json.loads(ver_str)
-            print(f"Version: {ver['version']}")
-        else:
-            print(f"unknown version: {ver_str}")
-    elif action == "models":
-        models_str = list_models(url)
-        if models_str:
-            models = json.loads(models_str)
-            index = 1
-            for m in models['data']:
-                print(f"Model[{index}]: {m['id']}, {m['object']}")
-                index += 1
-        else:
-            print(f"unknown models: {models_str}")
-    elif action == "completions" or action == "chat-completions":
-        input = RequestInput(
-            api_url = url,
-            prompt = args.prompt,
-            model = args.model_name,
-            output_len = args.output_len,
-            stream = args.stream,
-            ignore_eos = args.ignore_eos,
-            best_of = args.best_of,
-        )
-        print(input)
+    if "completions" in parsed_url.path:
+        print("Call API: OpenAI completions")
+        ctx = CompletionContext(prompt = args.prompt, model=args.model_name, max_tokens=args.output_len, stream=args.stream, ignore_eos=args.ignore_eos,)
         start_time = time.time()
-        result = None
-        if args.repeat > 1:
-            async def _run_requests_in_batch(num):
-                tasks: List[asyncio.Task] = []
-                for _ in range(num):
-                    tasks.append(asyncio.create_task(async_request_openai_completions(action, input, api_key=args.api_key)))
-                await asyncio.gather(*tasks)
-            asyncio.run(_run_requests_in_batch(args.repeat))
+        asyncio.run(async_request_openai_completions(ctx, args.url, args.api_key))
+        duration = time.time() - start_time
+        print(f"DONE in {duration:.3f} seconds")
+        if ctx.error:
+            print(f"ERROR: {ctx.error}")
         else:
-            result = asyncio.run(async_request_openai_completions(action, input, api_key=args.api_key))
-        end_time = time.time()
-        #print(f"Time elapsed: {datetime.fromtimestamp(end_time - start_time).strftime('%H:%M:%S.%f')}")
-        print(f"\n Time elapsed: {end_time - start_time} seconds.")
-        if result is not None:
-            if result.success:
-                print(f"Final generated text: {result.generated_text}")
-            else:
-                print(f"Error: {result.error}")
-    elif action == "embeddings" or action == "compatible-embeddings":
-        if args.url is None or args.model_name is None or args.prompt is None:
-            logger.error("Invalid embeddings paramters, required arguments: url, model-name, prompt")
-            return
-        result = asyncio.run(async_generate_embeddings(url, args.prompt, args.model_name, args.api_key))
-        logger.info(f"Embbedings result: {result}")
+            print(f"Generated: {ctx.generated}")
+            print(f"E2E Latency: {ctx.e2e_latency:0.3f}, TTFT: {ctx.ttft:0.3f}")
+    elif "embeddings" in parsed_url.path:
+        print("Call API: embeddings")
+        if args.prompt is None:
+            raise ValueError("Invalid prompt to call embeddings API")
+        result = asyncio.run(async_generate_embeddings(args.url, args.prompt, args.model_name, args.api_key))
+        print(f"Embeddings response: {result}")
+    elif "models" in parsed_url.path:
+        print(f"Model: {get_model(args.url)}")
+    elif "version" in parsed_url.path:
+        print(f"Version: {get_version(args.url)}")
+    elif "health" in parsed_url.path:
+        print(f"Health check: {check_health(args.url)}")
     else:
-        print(f"execute GET method: {url}")
-        ret = exec_get_method(args)
-        print(ret)
-        return
-    return
+        res = requests.get(args.url, headers=headers, timeout=10)
+        print(f"Response: {res}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Demonstration about llm openapi.")
-    parser.add_argument("--url", type=str, help="The host URL of the llm openapi server.", default="http://localhost:8000")
-    parser.add_argument("--api-key", type=str, help="The secret key to connect server.", default="")
-    parser.add_argument("--backend", type=str, help="The backend of the llm openapi server.", default="vllm", choices=["vllm", "trtllm"])
-    parser.add_argument("--path", type=str, help=f"The URL path of the llm openapi server. Its values can be: {VLLM_ENDPOINTS}", default="/v1/completions")
+    parser.add_argument("--url", type=str, help="The host URL of the llm openapi server.", default="http://localhost:8000/v1/completions")
+    parser.add_argument("--api-key", type=str, help="The secret key to connect server.")
     parser.add_argument("--prompt", type=str, help="The prompt for the completion.", default="The quick brown fox jumps over the lazy dog.")
-    parser.add_argument("--model-name", type=str, help="The model name for completions.", default="llama3-8b")
+    parser.add_argument("--model-name", type=str, help="The model name for completions.")
     parser.add_argument("--output-len", type=int, help="The maximum length of the output.", default=1024)
-    parser.add_argument("--ignore-eos", action="store_true", help="Force to ouput the maximum length of the output, ignore eos when found.", default=None)
-    parser.add_argument("--best-of", type=int, help="The number of best candicates in the sampling phase")
     parser.add_argument("--stream", action="store_true", help="Whether to stream the output or not.")
-    parser.add_argument("--repeat", type=int, help="The number to repeat the same requests.", default=1)
+    parser.add_argument("--ignore-eos", action="store_true", help="Force to ouput the maximum length of the output, ignore eos when found.", default=None)
 
     args = parser.parse_args()
     main(args)
