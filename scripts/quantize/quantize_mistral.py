e134165217e26c0ae5c225dd7bceb64259349ddd
linke <linke@pplabs.org>
2024-09-29 09:15:46 +0800

    Fix a minor quanization bug
    Signed-off-by: linke <linke@pplabs.org>

--- scripts/quantize/quantize_mistral.py
+++ scripts/quantize/quantize_mistral.py
@@ -4,19 +4,19 @@ from transformers import AutoTokenizer
 from auto_fp8 import AutoFP8ForCausalLM, BaseQuantizeConfig
 ## reference: https://huggingface.co/neuralmagic/Mistral-Nemo-Instruct-2407-FP8
 
-pretrained_model_dir = "mistralai/Mistral-Nemo-Instruct-2407"
-quantized_model_dir = "Mistral-Nemo-Instruct-2407-FP8"
+pretrained_model_dir = "/ppio1/models/MN-12B-Starcannon-v2"
+quantized_model_dir = "/ppio1/models/MN-12B-Starcannon-v2-FP8-2"
 
-tokenizer = AutoTokenizer.from_pretrained(pretrained_model_dir, use_fast=True, model_max_length=4096)
+tokenizer = AutoTokenizer.from_pretrained(pretrained_model_dir, use_fast=True, model_max_length=8192)
 tokenizer.pad_token = tokenizer.eos_token
 
-ds = load_dataset("mgoin/ultrachat_2k", split="train_sft").select(range(512))
+ds = load_dataset("mgoin/ultrachat_2k", split="train_sft").select(range(1024))
 examples = [tokenizer.apply_chat_template(batch["messages"], tokenize=False) for batch in ds]
 examples = tokenizer(examples, padding=True, truncation=True, return_tensors="pt").to("cuda")
 
 quantize_config = BaseQuantizeConfig(
     quant_method="fp8",
-    activation_scheme="static"
+    activation_scheme="static",
     ignore_patterns=["re:.*lm_head"],
 )
 
