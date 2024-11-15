e3e6b2d1519e747317e13e0310cedd10260b8855
linke <linke@pplabs.org>
2024-11-15 11:23:16 +0800

    Plot the speedup rate with best throughput
    Signed-off-by: linke <linke@pplabs.org>

--- test/plot_acc_rate.py
+++ test/plot_acc_rate.py
@@ -1,11 +1,12 @@
 import os
 import argparse
+import re
 import matplotlib.pyplot as plt
 from typing import List, Optional, Tuple
 from dataclasses import dataclass, field
 
-DEF_PLOT_BS=["all", "1", "4", "8", "10", "15"]
-DEF_PLOT_LENGTH=["all", "1000", "3000", "5000"]
+DEF_PLOT_BS=["all", "best", "1", "4", "8", "10", "15"]
+DEF_PLOT_LENGTH=["all", "1000", "3000", "5000", "6000"]
 
 @dataclass
 class MetricsData:
@@ -25,12 +26,15 @@ class MetricsData:
     raw_ttft: Optional[List[float]] = field(default_factory=list)
     raw_tpot: Optional[List[float]] = field(default_factory=list)
     raw_tps: Optional[List[float]] = field(default_factory=list)
+    ## others
+    extra_bs: Optional[List[int]] = field(default_factory=list)
 
     def get_plot_label(self):
         return self.model + "@" + self.name
 
-def load_metrics_from_file(file_path: str, update_model_name: bool) -> List[MetricsData]:
+def load_metrics_from_file(file_path: str, update_model_name: bool): # -> Tuple(List[MetricsData], List[MetricsData]):
     metrics=[]
+    best_metrics = []
     print(f"Load metrics from {file_path}")
     if not os.path.isfile(file_path):
         raise RuntimeError(f"Invalid metrics file path: {file_path}")
@@ -52,79 +56,104 @@ def load_metrics_from_file(file_path: str, update_model_name: bool) -> List[Metr
     with open(file_path, "r") as f:
         line = f.readline()
         cur_metrics = None
+        cur_best_metrics = None
+        scan_best_throughput = False
         while line:
             line = line.strip()
             if line.startswith("[BeginMetrics]"):
                 cur_metrics = MetricsData(name=name)
+                line = f.readline()
+                continue
+            elif line.startswith("[Best Throughput]"):
+                scan_best_throughput = True
+                line = f.readline()
+                continue
+            if scan_best_throughput:
+                match = re.search(r'\((\d+),(\d+)\)', line)
+                if match:
+                    cur_best_metrics = MetricsData(name=name)
+                    cur_best_metrics.model = model_name
+                    cur_best_metrics.input_len = int(match.group(1))
+                    cur_best_metrics.output_len = int(match.group(2))
+                    cur_best_metrics.ttft = []
+                    cur_best_metrics.extra_bs = []
+                    cur_best_metrics.tps = []
+                    cur_best_metrics.throughput = []
+                    best_metrics.append(cur_best_metrics)
+                elif line.startswith("[avg]") or line.startswith("[p50]") or line.startswith("[p90]") or line.startswith("[p99]"):
+                    parts = line[6:].split(",")
+                    for part in parts:
+                        kv = part.split(": ")
+                        if "ttft" in kv[0]:
+                            cur_best_metrics.ttft.append(float(kv[1]))
+                        elif "bs" in kv[0]:
+                            cur_best_metrics.extra_bs.append(int(kv[1]))
+                        elif "tps" in kv[0]:
+                            cur_best_metrics.tps.append(float(kv[1]))
+                        elif "throughput" in kv[0]:
+                            cur_best_metrics.throughput.append(int(kv[1]))
             else:
                 if cur_metrics is None:
                     line = f.readline()
                     continue
-            if line.startswith("[EndMetrics]"):
-                metrics.append(cur_metrics)
-                cur_metrics = None
-            if line.startswith("model"):
-                if model_name is None:
-                    cur_metrics.model = line.split(":")[1].strip()
-                else:
+                if line.startswith("[EndMetrics]"):
+                    metrics.append(cur_metrics)
+                    cur_metrics = None
+                if line.startswith("model"):
+                    if model_name is None:
+                        model_name = line.split(":")[1].strip()
                     cur_metrics.model = model_name
-            elif line.startswith("sequence-length"):
-                line = line.split(":")[1].strip()
-                lens = line.split(",")
-                cur_metrics.input_len = int(lens[0].strip())
-                cur_metrics.output_len = int(lens[1].strip())
-            elif line.startswith("batch-size"):
-                cur_metrics.batchsize = int(line.split(":")[1].strip())
-            elif line.startswith("e2e-latency"):
-                cur_metrics.latency = eval("[" + line.split(":")[1].strip() + "]")
-            elif line.startswith("ttft"):
-                cur_metrics.ttft = eval("[" + line.split(":")[1].strip() + "]")
-            elif line.startswith("tpot"):
-                cur_metrics.tpot = eval("[" + line.split(":")[1].strip() + "]")
-            elif line.startswith("tps"):
-                cur_metrics.tps = eval("[" + line.split(":")[1].strip() + "]")
-            elif line.startswith("throughput"):
-                line = line.split(":")[1].strip()
-                thp = line.split(",")
-                cur_metrics.throughput = [float(thp[0].strip()), float(thp[1].strip())]
-                cur_metrics.throughput.insert(0, cur_metrics.throughput[0] + cur_metrics.throughput[1])
-            elif line.startswith("rps"):
-                cur_metrics.rps = float(line.split(":")[1].strip())
-            elif line.startswith("raw-ttft"):
-                cur_metrics.raw_ttft = eval(line.split(":")[1].strip())
-            elif line.startswith("raw-tpot"):
-                cur_metrics.raw_tpot = eval(line.split(":")[1].strip())
-            elif line.startswith("raw-tps"):
-                cur_metrics.raw_tps = eval(line.split(":")[1].strip())
+                elif line.startswith("sequence-length"):
+                    line = line.split(":")[1].strip()
+                    lens = line.split(",")
+                    cur_metrics.input_len = int(lens[0].strip())
+                    cur_metrics.output_len = int(lens[1].strip())
+                elif line.startswith("batch-size"):
+                    cur_metrics.batchsize = int(line.split(":")[1].strip())
+                elif line.startswith("e2e-latency"):
+                    cur_metrics.latency = eval("[" + line.split(":")[1].strip() + "]")
+                elif line.startswith("ttft"):
+                    cur_metrics.ttft = eval("[" + line.split(":")[1].strip() + "]")
+                elif line.startswith("tpot"):
+                    cur_metrics.tpot = eval("[" + line.split(":")[1].strip() + "]")
+                elif line.startswith("tps"):
+                    cur_metrics.tps = eval("[" + line.split(":")[1].strip() + "]")
+                elif line.startswith("throughput"):
+                    line = line.split(":")[1].strip()
+                    thp = line.split(",")
+                    cur_metrics.throughput = [float(thp[0].strip()), float(thp[1].strip())]
+                    cur_metrics.throughput.insert(0, cur_metrics.throughput[0] + cur_metrics.throughput[1])
+                elif line.startswith("rps"):
+                    cur_metrics.rps = float(line.split(":")[1].strip())
+                elif line.startswith("raw-ttft"):
+                    cur_metrics.raw_ttft = eval(line.split(":")[1].strip())
+                elif line.startswith("raw-tpot"):
+                    cur_metrics.raw_tpot = eval(line.split(":")[1].strip())
+                elif line.startswith("raw-tps"):
+                    cur_metrics.raw_tps = eval(line.split(":")[1].strip())
             line = f.readline()
-    return metrics
+    return (metrics, best_metrics)
 
 def find_metrics(metrics, input_len, bs):
     for m in metrics:
-        if m.batchsize == bs and m.input_len == input_len:
-            return m
+        if bs is None:
+            if m.input_len == input_len:
+                return m
+        else:
+            if m.batchsize == bs and m.input_len == input_len:
+                return m
     return None
 
-def main(args: argparse.Namespace):
-    if not args.base:
-        raise ValueError("The baseline is not set")
-    if not args.targets:
-        raise ValueError("The targets are not set")
-    metrics_base = load_metrics_from_file(args.base, True)
-    targets = args.targets.split(",")
-    metrics_targets = []
-    for t in targets:
-        metrics_targets.append(load_metrics_from_file(t, True))
-    print(f"baseline: {metrics_base[0].get_plot_label()}")
-    for i in range(len(metrics_targets)):
-        print(f"target[{i}]: {metrics_targets[i][0].get_plot_label()}")
+def plot_with_bs(metrics_base, metrics_targets, args):
     plt_bs = args.plot_bs.split(",")
     if "all" in plt_bs:
-        plt_bs = DEF_PLOT_BS[1:]
+        plt_bs = DEF_PLOT_BS[2:]
     plt_length = args.plot_length.split(",")
     if "all" in plt_length:
         plt_length = DEF_PLOT_LENGTH[1:]
     print(f"plt_bs: {plt_bs}, plt_length: {plt_length}")
+    for i in range(len(metrics_targets)):
+        print(f"target[{i}]: {metrics_targets[i][0].get_plot_label()}")
 
     plt_data = []
     for i in range(len(metrics_targets)):
@@ -146,7 +175,7 @@ def main(args: argparse.Namespace):
         if args.plot_ann:
             for i, y in enumerate(data["y"]):
                 plt.annotate(f"{data['y'][i]:0.2f}", xy=(data["x"][i], data["y"][i]), xytext=(data["x"][i], data["y"][i]))
-    plt.title(f"Accelate rate to: {metrics_base[0].get_plot_label()}")
+    plt.title(f"Speedup vs: {metrics_base[0].get_plot_label()}")
     plt.xlabel("batch size")
     plt.ylabel("speedup")
     plt.legend()
@@ -155,6 +184,64 @@ def main(args: argparse.Namespace):
         plt.savefig(args.output, dpi=300)
     else:
         plt.show()
+
+def plot_with_best(best_metrics_base, best_metrics_targets, args):
+    print(f"best_metrics_base[{best_metrics_base[0].get_plot_label()}]: {best_metrics_base[0].input_len}, {best_metrics_base[0].ttft}, {best_metrics_base[0].tps}, {best_metrics_base[0].extra_bs}, {best_metrics_base[0].throughput}")
+    for i in range(len(best_metrics_targets)):
+        print(f"target[{i}]: {best_metrics_targets[i][0].get_plot_label()}")
+    plt_length = args.plot_length.split(",")
+    if "all" in plt_length:
+        plt_length = DEF_PLOT_LENGTH[1:]
+    ## x: length(1000, 3000, 5000), y: rate (p90)
+    plt_data = []
+    x_ticks = [int(i) for i in plt_length]
+    for i in range(len(best_metrics_targets)):
+        target = best_metrics_targets[i]
+        plt_data.append({"label": target[0].get_plot_label(), "x": [], "y": [], "ann": []})
+        for length in plt_length:
+            length = int(length)
+            mb = find_metrics(best_metrics_base, length, None)
+            mt = find_metrics(target, length, None)
+            if mb is None or mt is None:
+                raise RuntimeError(f"Not found right metrics data, length: {length}, base: {m_b}, target: {m_t}")
+            plt_data[-1]["x"].append(length)
+            plt_data[-1]["y"].append(mt.throughput[3] / mb.throughput[3])
+            plt_data[-1]["ann"].append(f"{plt_data[-1]['y'][-1]:.2f}({mt.extra_bs[3]}/{mb.extra_bs[3]})")
+    print(f"plting data: {plt_data}")
+    for data in plt_data:
+        plt.plot(data["x"], data["y"], label=data["label"])
+        if args.plot_ann:
+            for i, y in enumerate(data["y"]):
+                plt.annotate(data["ann"][i], xy=(data["x"][i], data["y"][i]), xytext=(data["x"][i], data["y"][i]))
+    plt.title(f"Speedup vs: {best_metrics_base[0].get_plot_label()}")
+    plt.xlabel("Context length")
+    plt.ylabel("Speedup")
+    plt.xticks(x_ticks)
+    plt.legend()
+    plt.grid(True)
+    if args.output:
+        plt.savefig(args.output, dpi=300)
+    else:
+        plt.show()
+
+def main(args: argparse.Namespace):
+    if not args.base:
+        raise ValueError("The baseline is not set")
+    if not args.targets:
+        raise ValueError("The targets are not set")
+    (metrics_base, best_metrics_base) = load_metrics_from_file(args.base, True)
+    targets = args.targets.split(",")
+    metrics_targets = []
+    best_metrics_targets = []
+    for t in targets:
+        (m1, m2) = load_metrics_from_file(t, True)
+        metrics_targets.append(m1)
+        best_metrics_targets.append(m2)
+    print(f"baseline: {metrics_base[0].get_plot_label()}")    
+    if "best" in args.plot_bs:
+        plot_with_best(best_metrics_base, best_metrics_targets, args)
+    else:
+        plot_with_bs(metrics_base, metrics_targets, args)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
